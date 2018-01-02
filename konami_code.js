e76e73bc2364a777d363e526de685faa19da69cc
Ed Wright <ew_wrightiii@live.com>
2018-01-02 05:34:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,9 @@ body.addEventListener('keydown', function(e){
   const key = parseInt(e.detail || e.which);
   if(key === code[index]){
     index++;
-    
+    if(index == code.length){
+      alert("Congratulations!");
+    }
   }
 })
 }
