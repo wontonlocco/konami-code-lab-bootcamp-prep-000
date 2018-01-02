8b4994dfc6921706fbe1c342561fd2bd0b905420
Ed Wright <ew_wrightiii@live.com>
2018-01-02 05:54:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-const body = document.getElementById('body');
+const input = document.getElementById('body');
 let index = 0;
 body.addEventListener('keydown', function(e){
   const key = parseInt(e.detail || e.location);
