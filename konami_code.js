9b6ebec6df5b959cf83623bb141e70651c757ca8
Ed Wright <ew_wrightiii@live.com>
2018-01-02 06:00:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
 let index = 0;
-function downKeyHandler(){
+function downKeyHandler(e){
   const key = parseInt(e.detail || e.location);
-  
+  console.log(e.detail)
   if(key === code[index]){
     index++;
     if(index == code.length){
