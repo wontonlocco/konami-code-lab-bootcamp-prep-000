1da920489fabd5c3702411970ffd77ddd6bf07a2
Ed Wright <ew_wrightiii@live.com>
2018-01-02 05:59:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
 let index = 0;
-body.addEventListener('keydown', function(e){
+function downKeyHandler(){
   const key = parseInt(e.detail || e.location);
+  
   if(key === code[index]){
     index++;
     if(index == code.length){
@@ -14,7 +15,8 @@ body.addEventListener('keydown', function(e){
   }else{
     index = 0;
   }
-})
+}
+
 function init() {
   const input = document.getElementById('body');
 
